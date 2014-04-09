3b33be1a3f3343e3a754265690b9868c160b8acf
bear <appuebear@gmail.com>
2014-04-09 18:17:09 +0800

    modify gulpfile add handlebars

--- gulpfile.js
+++ gulpfile.js
@@ -10,36 +10,40 @@ var appueConfig = {
 	// 项目目录
 	projects: ['prj-test'],
 	// js文件
-	scripts: '/**/*js'
+	scripts: '/**/*js',
+	// hbs 模板
+	hbs: '/**/*hbs'
 };
 
 // 循环项目run task
 appueConfig.projects.forEach(function(prj){
 
-	var paths = prj+appueConfig.scripts;
+	var pathsJS  = prj+appueConfig.scripts,
+		pathsHBS = prj+appueConfig.hbs;
 
 	// 压缩代码
 	gulp.task('compress', function(){
-		gulp.src(paths)
+		gulp.src(pathsJS)
 			.pipe(uglify({
 				outSourceMap: false
 			}))
 			.pipe(gulp.dest('build/'+prj))
 	});
 
+	// handlebars 模板
+	gulp.task('templates', function(){
+		gulp.src(pathsHBS)
+			.pipe(handlebars())
+			.pipe(defineModule('amd'))
+			.pipe(gulp.dest('build/'+prj));
+	});
+
 	// 监视项目
 	gulp.task('watch', function() {
-		gulp.watch(paths, ['compress']);
+		gulp.watch(pathsJS, ['compress']);
+		gulp.watch(pathsHBS, ['templates']);
 	});
 });
 
-// handlebars 模板
-gulp.task('templates', function(){
-  gulp.src(['templates/*.hbs'])
-    .pipe(handlebars())
-    .pipe(defineModule('node'))
-    .pipe(gulp.dest('build/templates/'));
-});
-
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['compress', 'watch']);
+gulp.task('default', ['compress', 'templates', 'watch']);
