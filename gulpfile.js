2a9dba04d28f9c8faf011e3636c58b7b07de6519
bear <appuebear@gmail.com>
2014-04-10 17:08:38 +0800

    add modules

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,14 @@ var gulp 		 = require('gulp'),
 	rename 		 = require("gulp-rename"), // 修改文件名 
 	zip 		 = require('gulp-zip'), // 文件压缩包
 	jshint 		 = require('gulp-jshint'), // JS文件效验
-	stylish 	 = require('jshint-stylish'); // JS效验报错
+	stylish 	 = require('jshint-stylish'), // JS效验报错
+	clean 		 = require('gulp-clean'), // 文件清理
+	map 		 = require('map-stream'); 
 
 // 初始化配置
 var appueConfig = {
 	projects: ['prj-test'], // 项目目录
+	allfiles: '/**/*', // 所有文件
 	html: '/**/*html', // HTML目录
 	scripts: '/**/*js', // js文件
 	css: '/**/*css', // CSS文件
@@ -20,12 +23,22 @@ var appueConfig = {
 // 循环项目run task
 appueConfig.projects.forEach(function(prj){
 
-	var pathHTML = prj+appueConfig.html, // HTML目录
+	var pathPRJ = 'build/'+prj;
+
+	var pathALL  = pathPRJ+appueConfig.allfiles, // 所有文件
+		pathHTML = prj+appueConfig.html, // HTML目录
 		pathJS   = prj+appueConfig.scripts, // JS目录
 		pathCSS  = prj+appueConfig.css, // CSS目录
 		pathHBS  = prj+appueConfig.hbs; // handlebars目录
 
-	var pathPRJ = 'build/'+prj;
+
+	// clean files
+	gulp.task('cleanfile', function(){
+		gulp.src(['build/', 'zip/'], {
+				read: false
+			})
+			.pipe(clean());
+	});
 
 	// 移动html文件
 	gulp.task('move-html', function(){
@@ -69,10 +82,27 @@ appueConfig.projects.forEach(function(prj){
 			.pipe(jshint.reporter(stylish))
 			.pipe(jshint.reporter('fail'));
 	});
+	// var myReporter = map(function (file, callback) {
+	// 	if (!file.jshint.success) {
+	// 		console.log('JSHINT fail in '+file.path);
+	// 		file.jshint.results.forEach(function (err) {
+	// 			if (err) {
+	// 				console.log(' '+file.path + ': line ' + err.line + ', col ' + err.character + ', code ' + err.code + ', ' + err.reason);
+	// 			}
+	// 		});
+	// 	}
+	// 	callback(null, file);
+	// });
+	// gulp.task('lint', function() {
+	// 	gulp.src(pathJS)
+	// 		.pipe(jshint())
+	// 		.pipe(myReporter);
+	// });
+
 
 	// 打包文件
 	gulp.task('zipfile', function () {
-	    gulp.src(pathPRJ+'/**/*')
+	    gulp.src(pathALL)
 	        .pipe(zip(prj+'.zip'))
 	        .pipe(gulp.dest('zip'));
 	});
@@ -87,8 +117,11 @@ appueConfig.projects.forEach(function(prj){
 });
 
 // 建立生产 $ gulp build
-gulp.task('build', ['lint', 'move-html', 'compress', 'minify-css', 'templates', 'watch']);
+// gulp.task('build', ['lint', 'move-html', 'compress', 'minify-css', 'templates', 'watch']);
+gulp.task('build', ['cleanfile', 'lint', 'move-html', 'compress', 'minify-css', 'templates']);
 // JS check
 gulp.task('check', ['lint']);
+// clean files
+gulp.task('clean', ['cleanfile']);
 // 打包文件 $ gulp zip
 gulp.task('zip', ['zipfile']);
