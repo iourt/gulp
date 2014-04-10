c5e0a5e9b02b307f4d6d81d5a9ef4678b8580437
bear <appuebear@gmail.com>
2014-04-10 15:23:30 +0800

    modify files

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,81 @@
 var gulp = require('gulp'),
-	// 引入JS压缩插件
-	uglify = require('gulp-uglify'),
-	// 引入handlebars
-	handlebars = require('gulp-handlebars'),
-	defineModule = require('gulp-define-module'); 
+	uglify = require('gulp-uglify'), // 引入JS压缩插件
+	minifyCSS = require('gulp-minify-css'), // 引入CSS压缩
+	handlebars = require('gulp-handlebars'), // 引入handlebars
+	defineModule = require('gulp-define-module'),
+	rename = require("gulp-rename"), // 修改文件名 
+	zip = require('gulp-zip'); // 文件压缩包
 
 // 初始化配置
 var appueConfig = {
-	// 项目目录
-	projects: ['prj-test'],
-	// js文件
-	scripts: '/**/*js',
-	// hbs 模板
-	hbs: '/**/*hbs'
+	projects: ['prj-test'], // 项目目录
+	html: '/**/*html', // HTML目录
+	scripts: '/**/*js', // js文件
+	css: '/**/*css', // CSS文件
+	hbs: '/**/*hbs' // hbs 模板
 };
 
 // 循环项目run task
 appueConfig.projects.forEach(function(prj){
 
-	var pathsJS  = prj+appueConfig.scripts,
-		pathsHBS = prj+appueConfig.hbs;
+	var pathHTML = prj+appueConfig.html, // HTML目录
+		pathJS   = prj+appueConfig.scripts, // JS目录
+		pathCSS  = prj+appueConfig.css, // CSS目录
+		pathHBS  = prj+appueConfig.hbs; // handlebars目录
+
+	var pathPRJ = 'build/'+prj;
+
+	// 移动html文件
+	gulp.task('move-html', function(){
+		gulp.src(pathHTML)
+			.pipe(gulp.dest(pathPRJ));
+	});
 
 	// 压缩代码
 	gulp.task('compress', function(){
-		gulp.src(pathsJS)
+		gulp.src(pathJS)
 			.pipe(uglify({
 				outSourceMap: false
 			}))
-			.pipe(gulp.dest('build/'+prj))
+			.pipe(gulp.dest(pathPRJ));
+	});
+
+	// 压缩CSS文件
+	gulp.task('minify-css', function(){
+		gulp.src(pathCSS)
+			.pipe(minifyCSS())
+			.pipe(gulp.dest(pathPRJ));
 	});
 
 	// handlebars 模板
 	gulp.task('templates', function(){
-		gulp.src(pathsHBS)
+		gulp.src(pathHBS)
 			.pipe(handlebars())
 			.pipe(defineModule('amd'))
-			.pipe(gulp.dest('build/'+prj));
+			.pipe(rename(function(path){
+				// path.dirname += '';
+				path.basename += ".hbs";
+				path.extname = ".js"
+			}))
+			.pipe(gulp.dest(pathPRJ));
+	});
+
+	gulp.task('zip', function () {
+	    gulp.src(pathPRJ+'/**/*')
+	        .pipe(zip(prj+'.zip'))
+	        .pipe(gulp.dest('zip'));
 	});
 
 	// 监视项目
 	gulp.task('watch', function() {
-		gulp.watch(pathsJS, ['compress']);
-		gulp.watch(pathsHBS, ['templates']);
+		gulp.watch(pathHTML, ['move-html']);
+		gulp.watch(pathJS, ['compress']);
+		gulp.watch(pathCSS, ['minify-css']);
+		gulp.watch(pathHBS, ['templates']);
 	});
 });
 
-// The default task (called when you run `gulp` from cli)
-gulp.task('release', ['compress', 'templates', 'watch']);
-gulp.task('build', ['compress', 'templates', 'watch']);
+// 默认执行任务 $ gulp
+gulp.task('default', ['move-html', 'compress', 'minify-css', 'templates', 'watch']);
+
+gulp.task('zipfile', ['move-html', 'compress', 'minify-css', 'templates', 'zip']);
