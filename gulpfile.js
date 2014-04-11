199d667b79f670254d91cd1b34e765aecb82d22a
bear <appuebear@gmail.com>
2014-04-11 12:32:09 +0800

    modify gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -22,10 +22,12 @@ var appueConfig = {
 	hbs: '/**/*hbs' // hbs 模板
 };
 
+var pathBase = 'build/';
+
 // 循环项目run task
 appueConfig.projects.forEach(function(prj){
 
-	var pathPRJ = 'build/'+prj;
+	var pathPRJ = pathBase+prj;
 
 	var pathALL  = pathPRJ+appueConfig.allfiles, // 所有文件
 		pathHTML = prj+appueConfig.html, // HTML目录
@@ -35,65 +37,63 @@ appueConfig.projects.forEach(function(prj){
 
 
 	// clean files
-	gulp.task('cleanfile', function(){
-		console.log(1);
-		gulp.src(['build/', 'zip/'], {read: false})
-			.pipe(clean());
+	gulp.task('clean', function(){
+		return gulp.src(['build/', 'zip/'], {read: false})
+					.pipe(clean());
 	});
 
 	// 移动html文件
 	gulp.task('move-html', function(){
-		console.log(2);
-		gulp.src(pathHTML)
-			.pipe(gulp.dest(pathPRJ));
+		return gulp.src(pathHTML)
+					.pipe(gulp.dest(pathPRJ));
 	});
 
 	// 压缩代码
 	gulp.task('compress', function(){
-		console.log(3);
-		gulp.src(pathJS)
-			.pipe(uglify({
-				outSourceMap: false
-			}))
-			.pipe(gulp.dest(pathPRJ));
+		return gulp.src(pathJS)
+					.pipe(uglify({
+						outSourceMap: false
+					}))
+					.pipe(gulp.dest(pathPRJ));
 	});
 
 	// 压缩CSS文件
 	gulp.task('minify-css', function(){
-		gulp.src(pathCSS)
-			.pipe(minifyCSS())
-			.pipe(gulp.dest(pathPRJ));
+		return gulp.src(pathCSS)
+					.pipe(minifyCSS())
+					.pipe(gulp.dest(pathPRJ));
 	});
 
 	// handlebars 模板
-	gulp.task('templates', function(){
-		gulp.src(pathHBS)
-			.pipe(handlebars())
-			.pipe(defineModule('amd'))
-			.pipe(rename(function(path){
-				// path.dirname += '';
-				path.basename += ".hbs";
-				path.extname = ".js"
-			}))
-			.pipe(gulp.dest(prj))
-			.pipe(gulp.dest(pathPRJ));
-		//gulp.src(pathHBS)
-		//	.pipe(handlebars())
-		//	.pipe(defineModule('amd'))
-		//	.pipe(rename(function(path){
-		//		// path.dirname += '';
-		//		path.basename += ".hbs";
-		//		path.extname = ".js"
-		//	}))
-		//	.pipe(gulp.dest(prj));
+	gulp.task('hbs_build', function(){
+		return gulp.src(pathHBS)
+					.pipe(handlebars())
+					.pipe(defineModule('amd'))
+					.pipe(rename(function(path){
+						// path.dirname += '';
+						path.basename += ".hbs";
+						path.extname = ".js"
+					}))
+					.pipe(gulp.dest(pathPRJ));
+	});
+	gulp.task('hbs_test', function(){
+		return gulp.src(pathHBS)
+					.pipe(handlebars())
+					.pipe(defineModule('amd'))
+					.pipe(rename(function(path){
+						// path.dirname += '';
+						path.basename += ".hbs";
+						path.extname = ".js"
+					}))
+					.pipe(gulp.dest(prj));
 	});
 
 	// JS 文件效验
-	gulp.task('lint', function() {
-		gulp.src(pathJS)
-			.pipe(jshint())
-			.pipe(jshint.reporter(stylish))
-			.pipe(jshint.reporter('fail'));
+	gulp.task('check', function() {
+		return gulp.src(pathJS)
+					.pipe(jshint())
+					.pipe(jshint.reporter(stylish))
+					.pipe(jshint.reporter('fail'));
 	});
 	// var myReporter = map(function (file, callback) {
 	// 	if (!file.jshint.success) {
@@ -114,18 +114,22 @@ appueConfig.projects.forEach(function(prj){
 
 
 	// 打包文件
-	gulp.task('zipfile', function () {
-	    gulp.src(pathALL)
-	        .pipe(zip(prj+'.zip'))
-	        .pipe(gulp.dest('zip'));
+	gulp.task('zip', function () {
+	    return gulp.src(pathALL)
+	        		.pipe(zip(prj+'.zip'))
+	        		.pipe(gulp.dest('zip'));
 	});
 
 	// 监视项目
-	gulp.task('watch', function() {
-		gulp.watch(pathALL, ['cleanfile']);
+	gulp.task('watch_build', function() {
+		gulp.watch(pathALL, ['clean']);
 		gulp.watch(pathHTML, ['move-html']);
 		gulp.watch(pathJS, ['compress']);
 		gulp.watch(pathCSS, ['minify-css']);
+		gulp.watch(pathHBS, ['templates']);
+	});
+	gulp.task('watch_test', function() {
+		gulp.watch(pathJS, ['lint'])
 		gulp.watch(pathHBS, ['templates']);
 	});
 });
@@ -144,12 +148,7 @@ gulp.task('connect', function () {
 
 // 建立生产 $ gulp build
 // gulp.task('build', ['lint', 'move-html', 'compress', 'minify-css', 'templates', 'watch']);
-gulp.task('build', ['cleanfile', 'lint', 'move-html', 'compress', 'minify-css', 'templates']);
-// JS check
-gulp.task('check', ['lint']);
-// clean files
-gulp.task('clean', ['cleanfile']);
-// 打包文件 $ gulp zip
-gulp.task('zip', ['zipfile']);
+gulp.task('build', ['clean', 'check', 'move-html', 'compress', 'minify-css', 'hbs_build']);
+
 // run 服务器
-gulp.task('run', ['build', 'connect', 'watch']);
+gulp.task('run', ['check', 'hbs_test', 'connect', 'watch_test']);
